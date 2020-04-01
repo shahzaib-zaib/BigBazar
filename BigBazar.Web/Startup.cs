fdf929f4bbcb424dff69cc8d3d41ae29480d6490
Shahzaib <shahzaibux@gmail.com>
2020-04-01 17:07:45 +0500

    Add project files.

---
+++ BigBazar.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BigBazar.Web.Startup))]
+namespace BigBazar.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
