751e6b955fa3419bc90e80fe5d708d2137e935aa
Shahzaib <shahzaibux@gmail.com>
2020-04-01 20:18:48 +0500

    Added Product and Category Business Entities and Database

---
+++ BigBazar.Database/BDContext.cs
@@ -0,0 +1,19 @@
+﻿using BigBazar.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BigBazar.Database
+{
+    public class BDContext : DbContext
+    {
+        public BDContext() : base("BigBazarConnection")
+        {
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
