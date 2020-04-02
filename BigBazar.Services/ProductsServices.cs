87aaa58278f6548128002423d4207d9bcc361fbe
Shahzaib <shahzaibux@gmail.com>
2020-04-03 04:54:20 +0500

    Create Product Table and Controller

---
+++ BigBazar.Services/ProductsServices.cs
@@ -0,0 +1,54 @@
+﻿using BigBazar.Database;
+using BigBazar.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BigBazar.Services
+{
+    public class ProductsService
+    {
+        public Product GetProduct(int ID)
+        {
+            using (var context = new BDContext())
+            {
+                return context.Products.Find(ID);
+            }
+        }
+        public List<Product> GetProducts()
+        {
+            using (var context = new BDContext())
+            {
+                return context.Products.ToList();
+            }
+        }
+        public void SaveProduct(Product product)
+        {
+            using (var context = new BDContext())
+            {
+                context.Products.Add(product);
+                context.SaveChanges();
+            }
+        }
+        public void UpdateProduct(Product product)
+        {
+            using (var context = new BDContext())
+            {
+                context.Entry(product).State = System.Data.Entity.EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+
+        public void DeleteProduct(int ID)
+        {
+            using (var context = new BDContext())
+            {
+                var product = context.Products.Find(ID);
+                context.Products.Remove(product);
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ BigBazar.Web/Controllers/ProductController.cs
@@ -0,0 +1,39 @@
+﻿using BigBazar.Entities;
+using BigBazar.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BigBazar.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        ProductsService productsService = new ProductsService();
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ProductTable()
+        {
+            var products = productsService.GetProducts();
+            return View(products);
+        }
+         [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+            productsService.SaveProduct(product);
+            return RedirectToAction("ProductTable");
+        }
+
+    }
+}
