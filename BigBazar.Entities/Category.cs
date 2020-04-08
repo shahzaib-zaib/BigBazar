963098e39ac446db24f859bb46b9c8d22f80992c
Shahzaib <shahzaibux@gmail.com>
2020-04-09 01:52:44 +0500

    Updated Project

--- BigBazar.Entities/Category.cs
+++ BigBazar.Entities/Category.cs
@@ -11,5 +11,6 @@ namespace BigBazar.Entities
         public string ImageURL { get; set; }
         public List<Product> Products { get; set; }
 
+        public bool isFeatured { get; set; }
     }
 }
--- BigBazar.Entities/Product.cs
+++ BigBazar.Entities/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +9,12 @@ namespace BigBazar.Entities
 {
     public class Product : BaseEntity
     {
+        [Range(1, 100000)]
         public decimal Price { get; set; }
-        //public int CategoryID { get; set; }
-        public virtual Category Category { get; set; }
-        
 
+        public int CategoryID { get; set; }
+        public virtual Category Category { get; set; }
+
+        public string ImageURL { get; set; }
     }
 }
