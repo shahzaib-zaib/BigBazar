fe57e71dd28d2d5c94d0bfedce65ff9e585d8411
Shahzaib <shahzaibux@gmail.com>
2020-04-10 01:46:49 +0500

    Updated Pages

--- BigBazar.Web/Models/IdentityModels.cs
+++ BigBazar.Web/Models/IdentityModels.cs
@@ -6,9 +6,12 @@ using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace BigBazar.Web.Models
 {
-    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        public string Name { get; set; }
+        public string Address { get; set; }
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
@@ -21,7 +24,7 @@ namespace BigBazar.Web.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("ClothBazar", throwIfV1Schema: false)
         {
         }
 
